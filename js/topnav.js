f3e41bee0b9a8eb0a05787438230766dc226e019
Matias Infante <maty.infante@gmail.com>
2018-08-23 11:59:20 -0300

    Matias

--- js/topnav.js
+++ js/topnav.js
@@ -1,12 +1,17 @@
-// JavaScript Document
-window.onscroll = function() {estatica()};
-var navbar = document.getElementById("navbar");
-var off = navbar.offsetTop;
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
 
-function estatica() {
-  if (window.pageYOffset >= off) {
-    navbar.classList.add("static");
+// Get the header
+var header = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("static");
   } else {
-    navbar.classList.remove("static");
-  }	
+    header.classList.remove("static");
+  }
 }
